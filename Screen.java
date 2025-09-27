ab486f4e5dcb6e11b5af2ad6b2d3506d3beab8da
small-j1337 <jerryliang.mv@gmail.com>
2025-09-27 14:20:40 -0700

    Import Images

--- Screen.java
+++ Screen.java
@@ -2,10 +2,21 @@ import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 
 public class Screen extends JPanel {
+	private BufferedImage background;
 	public Screen(){
-		
+		try {
+            background = ImageIO.read(new File("strawberry.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 	}
 	@Override
 	public Dimension getPreferredSize(){

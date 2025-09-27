9ea8e471ece34cede0e2bf360b4d51430273a9a2
small-j1337 <jerryliang.mv@gmail.com>
2025-09-27 15:06:26 -0700

    Update Screen.java

--- Screen.java
+++ Screen.java
@@ -9,22 +9,36 @@ import javax.imageio.ImageIO;
 
 
 public class Screen extends JPanel {
-	private BufferedImage background;
-	private BufferedImage chickenImage;
+	private BufferedImage background, chickenImage, cowImage, farmerImage, sheepImage, pigImage, scarecrowImage;
+
 	private DLList<Chicken> chicken;
+	private DLList<Cow> cow;
+	private DLList<Sheep> sheep;
+	private DLList<Pig> pig;
+	private DLList<Farmer> farmer;
+	private DLList<Scarecrow> scarecrow;
+
 	public Screen(){
 		chicken = new DLList<Chicken>();
+		cow = new DLList<Cow>();
+		sheep = new DLList<Sheep>();
+		pig = new DLList<Pig>();
+		farmer = new DLList<Farmer>();
+		scarecrow = new DLList<Scarecrow>();
+
 		try {
             background = ImageIO.read(new File("images/background.png"));
 			chickenImage = ImageIO.read(new File("images/chicken.png"));
+			cowImage = ImageIO.read(new File("images/cow.png"));
+			farmerImage = ImageIO.read(new File("images/farmer.png"));
+			sheepImage = ImageIO.read(new File("images/sheep.png"));
+			pigImage = ImageIO.read(new File("images/pig.png"));
+			scarecrowImage = ImageIO.read(new File("images/scarecrow.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-		for(int i=0; i<5; i++){
-			int x = (int)(Math.random()*600)+50;
-			int y = (int)(Math.random()*100)+350;
-			chicken.add(new Chicken(chickenImage,x,y));
-		}
+		//int x = (int)(Math.random()*600)+50;
+		//int y = (int)(Math.random()*100)+350;
 		
 	}
 	@Override
@@ -36,10 +50,5 @@ public class Screen extends JPanel {
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.drawImage(background,0,0,800,600,null);
-
-		for(int i=0; i<chicken.size(); i++){
-			chicken.get(i).drawMe(g);
-			System.out.println("X: " + chicken.get(i).getX() + " Y: " + chicken.get(i).getY());
-		}
 	}
 }

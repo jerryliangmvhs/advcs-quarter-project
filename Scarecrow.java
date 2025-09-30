5fb6d8921a2cff10e445906a5ef40ce8b17eb187
small-j1337 <jerryliang.mv@gmail.com>
2025-09-30 15:37:55 -0700

    Add MouseListener to Screen.java

--- Scarecrow.java
+++ Scarecrow.java
@@ -20,5 +20,19 @@ public class Scarecrow {
     public int getY(){
         return y;
     }
+    public void setX(int x){
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+    public boolean mouseEvent(int mouseX, int mouseY){
+        int imageWidth = 209;
+        int imageHeight = 171;
+        if(mouseX >= x && mouseX<= x+imageWidth && mouseY >= y && mouseY <= y + imageHeight){
+            return true;
+        }
+        return false;
+    }
 
 }
--- Screen.java
+++ Screen.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 
 
@@ -42,72 +44,72 @@ public class Screen extends JPanel implements ActionListener {
 		this.setLayout(null);
 
 		addChicken = new JButton("Add Chicken");
-		addChicken.setBounds(850,50,100,30);
+		addChicken.setBounds(825,50,150,30);
 		addChicken.addActionListener(this);
 		this.add(addChicken);
 
 		removeChicken = new JButton("Remove Chicken");
-		removeChicken.setBounds(850,85,100,30);
+		removeChicken.setBounds(825,85,150,30);
 		removeChicken.addActionListener(this);
 		this.add(removeChicken);
 
 		addCow = new JButton("Add Cow");
-		addCow.setBounds(850,120,100,30);
+		addCow.setBounds(825,120,150,30);
 		addCow.addActionListener(this);
 		this.add(addCow);
 
 		removeCow = new JButton("Remove Cow");
-		removeCow.setBounds(850,155,100,30);
+		removeCow.setBounds(825,155,150,30);
 		removeCow.addActionListener(this);
 		this.add(removeCow);
 
 		addFarmer = new JButton("Add Farmer");
-		addFarmer.setBounds(850,190,100,30);
+		addFarmer.setBounds(825,190,150,30);
 		addFarmer.addActionListener(this);
 		this.add(addFarmer);
 
 		removeFarmer = new JButton("Remove Farmer");
-		removeFarmer.setBounds(850,225,100,30);
+		removeFarmer.setBounds(825,225,150,30);
 		removeFarmer.addActionListener(this);
 		this.add(removeFarmer);
 
 		addPig = new JButton("Add Pig");
-		addPig.setBounds(850,260,100,30);
+		addPig.setBounds(825,260,150,30);
 		addPig.addActionListener(this);
 		this.add(addPig);
 
 		removePig = new JButton("Remove Pig");
-		removePig.setBounds(850,295,100,30);
+		removePig.setBounds(825,295,150,30);
 		removePig.addActionListener(this);
 		this.add(removePig);
 
 		addScarecrow = new JButton("Add Scarecrow");
-		addScarecrow.setBounds(850,335,100,30);
+		addScarecrow.setBounds(825,335,150,30);
 		addScarecrow.addActionListener(this);
 		this.add(addScarecrow);
 
 		removeScarecrow = new JButton("Remove Scarecrow");
-		removeScarecrow.setBounds(850,370,100,30);
+		removeScarecrow.setBounds(825,370,150,30);
 		removeScarecrow.addActionListener(this);
 		this.add(removeScarecrow);
 
 		addSheep = new JButton("Add Sheep");
-		addSheep.setBounds(850,405,100,30);
+		addSheep.setBounds(825,405,150,30);
 		addSheep.addActionListener(this);
 		this.add(addSheep);
 
 		removeSheep = new JButton("Remove Sheep");
-		removeSheep.setBounds(850,440,100,30);
+		removeSheep.setBounds(825,440,150,30);
 		removeSheep.addActionListener(this);
 		this.add(removeSheep);
 
-		scramble = new JButton("Scramble");
-		scramble.setBounds(850,475,100,30);
+		scramble = new JButton("Randomize");
+		scramble.setBounds(825,475,150,30);
 		scramble.addActionListener(this);
 		this.add(scramble);
 
 		clear = new JButton("Clear");
-		clear.setBounds(850,510,100,30);
+		clear.setBounds(825,510,150,30);
 		clear.addActionListener(this);
 		this.add(clear);
 
@@ -148,11 +150,130 @@ public class Screen extends JPanel implements ActionListener {
 		g.setColor(Color.BLACK);
 		g.fillRect(0,0,1000,600);
 		g.drawImage(background,0,0,800,600,null);
+
+		for(int i=0; i<chicken.size(); i++){
+			chicken.get(i).drawMe(g);
+		}
+		for(int i=0; i<cow.size(); i++){
+			cow.get(i).drawMe(g);
+		}
+		for(int i=0; i<farmer.size(); i++){
+			farmer.get(i).drawMe(g);
+		}
+		for(int i=0; i<pig.size(); i++){
+			pig.get(i).drawMe(g);
+		}
+		for(int i=0; i<scarecrow.size(); i++){
+			scarecrow.get(i).drawMe(g);
+		}
+		for(int i=0; i<sheep.size(); i++){
+			sheep.get(i).drawMe(g);
+		}
 	}
 
 	public void actionPerformed(ActionEvent e){
-
-
+		int x = (int)(Math.random()*600)+50;
+		int y = (int)(Math.random()*150)+300;
+		if(e.getSource()==addSheep){
+			sheep.add(new Sheep(sheepImage,x,y));
+		}
+		if(e.getSource()==removeSheep && sheep.size()>=1){
+			sheep.remove((int)(Math.random()*sheep.size()));
+		}
+		if(e.getSource()==addChicken){
+			chicken.add(new Chicken(chickenImage,x,y));
+		}
+		if(e.getSource()==removeChicken && chicken.size() >=1){
+			chicken.remove((int)(Math.random()*chicken.size()));
+		}
+		if(e.getSource()==addCow){
+			cow.add(new Cow(cowImage,x,y));
+		}
+		if(e.getSource()==removeCow && cow.size()>=1){
+			cow.remove((int)(Math.random()*cow.size()));
+		}
+		if(e.getSource()==addFarmer){
+			farmer.add(new Farmer(farmerImage,x,y));
+		}
+		if(e.getSource()==removeFarmer && farmer.size()>=1){
+			farmer.remove((int)(Math.random()*farmer.size()));
+		}
+		if(e.getSource()==addScarecrow){
+			scarecrow.add(new Scarecrow(scarecrowImage,x,y));
+		}
+		if(e.getSource()==removeScarecrow && scarecrow.size()>=1){
+			scarecrow.remove((int)(Math.random()*scarecrow.size()));
+		}
+		if(e.getSource()==addPig){
+			pig.add(new Pig(pigImage,x,y));
+		}
+		if(e.getSource()==removePig && pig.size()>=1){
+			pig.remove((int)(Math.random()*pig.size()));
+		}
+		if(e.getSource()==scramble){
+			for(int i=0; i<chicken.size(); i++){
+				int randX = (int)(Math.random()*600)+50;
+				int randY = (int)(Math.random()*150)+300;
+				chicken.get(i).setX(randX);
+				chicken.get(i).setY(randY);
+			}
+			for(int i=0; i<cow.size(); i++){
+				int randX = (int)(Math.random()*600)+50;
+				int randY = (int)(Math.random()*150)+300;
+				cow.get(i).setX(randX);
+				cow.get(i).setY(randY);
+			}
+			for(int i=0; i<farmer.size(); i++){
+				int randX = (int)(Math.random()*600)+50;
+				int randY = (int)(Math.random()*150)+300;
+				farmer.get(i).setX(randX);
+				farmer.get(i).setY(randY);
+			}
+			for(int i=0; i<pig.size(); i++){
+				int randX = (int)(Math.random()*600)+50;
+				int randY = (int)(Math.random()*150)+300;
+				pig.get(i).setX(randX);
+				pig.get(i).setY(randY);
+			}
+			for(int i=0; i<scarecrow.size(); i++){
+				int randX = (int)(Math.random()*600)+50;
+				int randY = (int)(Math.random()*150)+300;
+				scarecrow.get(i).setX(randX);
+				scarecrow.get(i).setY(randY);
+			}
+			for(int i=0; i<sheep.size(); i++){
+				int randX = (int)(Math.random()*600)+50;
+				int randY = (int)(Math.random()*150)+300;
+				sheep.get(i).setX(randX);
+				sheep.get(i).setY(randY);
+			}
+		}
+		if(e.getSource()==clear){
+			pig.clear();
+			cow.clear();
+			sheep.clear();
+			scarecrow.clear();
+			farmer.clear();
+			chicken.clear();
+		}
 		repaint();
 	}
+
+	public void mousePressed(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
+		repaint();
+		
+	}
+    public void mouseReleased(MouseEvent e) {}
+
+
+    public void mouseEntered(MouseEvent e) {}
+
+
+    public void mouseExited(MouseEvent e) {}
+
+
+    public void mouseClicked(MouseEvent e) {}
+
 }
